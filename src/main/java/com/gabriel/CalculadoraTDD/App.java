23a10a7efe1f259362624edfd8cd3c4a88d2846c
pikeno221 <gabriellgomesbh@gmail.com>
2018-09-03 00:12:02 -0300

    Criando classe CalculadoraTest... start!

---
+++ src/main/java/com/gabriel/CalculadoraTDD/App.java
@@ -0,0 +1,13 @@
+package com.gabriel.CalculadoraTDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/gabriel/CalculadoraTDD/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package com.gabriel.CalculadoraTDD;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CalculadoraTest {
+
+	@Test
+	public void deveSomarDoisValores() {
+		// cenario
+		int a = 5;
+		int b = 3;
+		
+		Calculadora calc = new Calculadora();
+		
+		// acao
+		int total = calc.somar(a, b);
+		
+		//verificacao
+		Assert.assertEquals(8.8, total);
+	}
+}
